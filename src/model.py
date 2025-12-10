bed42a7137490ef9fe6470fe53db65586ba9ba4f
coderboom-04 <103363668+coderboom-04@users.noreply.github.com>
2025-12-10 09:49:35 +0530

    model.py

--- src/model.py
+++ src/model.py
@@ -1,23 +1,23 @@
-import spacy
-import numpy as np
 import pandas as pd
-import streamlit as st
+import numpy as np
+import spacy
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import streamlit as st
 
 @st.cache_resource
 def load_spacy_model():
     return spacy.load("en_core_web_md")
 
-@st.cache_resource
-def embed_list(_nlp, cleaned_list):
-    return np.array([_nlp(text).vector for text in cleaned_list])
+@st.cache_resource(hash_funcs={pd.DataFrame: lambda _: None})
+def embed_list(_nlp, text_list):
+    return np.array([_nlp(text).vector for text in text_list])
 
 
 class RecipeRecommender:
     def __init__(self, dataframe):
         self.df = dataframe
-        
+
         # TF-IDF
         self.vectorizer = TfidfVectorizer(stop_words='english')
         self.tfidf_matrix = self.vectorizer.fit_transform(self.df['cleaned_ingredients'])
@@ -25,9 +25,10 @@ class RecipeRecommender:
         # spaCy model
         self.nlp = load_spacy_model()
 
-        # compute embeddings
+        # Precompute embeddings
         cleaned_list = self.df['cleaned_ingredients'].tolist()
         self.recipe_embeddings = embed_list(self.nlp, cleaned_list)
+
 
     def recommend_semantic(self, user_input, top_n=5):
         query_vec = self.nlp(user_input).vector.reshape(1, -1)
