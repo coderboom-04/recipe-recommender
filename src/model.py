bff053e1a8c6e9c6b77a0739279ee96a336b1c9e
coderboom-04 <103363668+coderboom-04@users.noreply.github.com>
2025-12-10 10:35:57 +0530

    model.py

--- src/model.py
+++ src/model.py
@@ -7,16 +7,11 @@ import streamlit as st
 
 @st.cache_resource
 def load_spacy_model():
-    try:
-        return spacy.load("en_core_web_sm")
-    except:
-        import spacy.cli
-        spacy.cli.download("en_core_web_sm")
-        return spacy.load("en_core_web_sm")
+    return spacy.load("en_core_web_sm")
+
 @st.cache_resource(hash_funcs={pd.DataFrame: lambda _: None})
 def embed_list(_nlp, text_list):
     return np.array([_nlp(text).vector for text in text_list])
-
 
 class RecipeRecommender:
     def __init__(self, dataframe):
@@ -32,7 +27,6 @@ class RecipeRecommender:
         # Precompute embeddings
         cleaned_list = self.df['cleaned_ingredients'].tolist()
         self.recipe_embeddings = embed_list(self.nlp, cleaned_list)
-
 
     def recommend_semantic(self, user_input, top_n=5):
         query_vec = self.nlp(user_input).vector.reshape(1, -1)
